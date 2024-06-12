d94a5d06729a3d6db4781075ed9590d8952e0637
Fgabor99 <119508736+Fgabor99@users.noreply.github.com>
2024-06-12 17:03:05 +0200

    Add files via upload

---
+++ CuntriesAndCities/src/main/java/org/example/cuntriesandcities/CuntriesAndCitiesApplication.java
@@ -0,0 +1,13 @@
+package org.example.cuntriesandcities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuntriesAndCitiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuntriesAndCitiesApplication.class, args);
+    }
+
+}
---
+++ CuntriesAndCities/src/test/java/org/example/cuntriesandcities/CuntriesAndCitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cuntriesandcities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuntriesAndCitiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
